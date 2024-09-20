528800c046d8348a890424d4d0d6070c5baf5083
harinisha-ramesh <harinisharamesh355@gmail.com>
2024-09-20 18:33:29 +0530

    update

--- dictionaries.py
+++ dictionaries.py
@@ -78,3 +78,32 @@ else:
 
 
 # CHANGE DICTIONARY ITEMS
+
+#Change Values
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+#Update Dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+
+# ADD DICTIONARY ITEMS
+
+#Adding Items
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
