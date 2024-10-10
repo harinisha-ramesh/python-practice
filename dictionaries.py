f17774c0951899a63feee92745ee720ee784789b
harinisha-ramesh <harinisharamesh355@gmail.com>
2024-10-10 10:55:09 +0530

    update

--- dictionaries.py
+++ dictionaries.py
@@ -107,3 +107,171 @@ thisdict = {
 thisdict["color"] = "red"
 print(thisdict)
 
+#update()
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+
+
+#REMOVE ITEMS
+
+#pop()
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+#popitem() -----> it will remove the last item
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+
+#del() ----> removes the item with the specific key name
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+#del() ----> can delete the dictionary completely
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+
+
+#clear() ----> empties the dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+
+#LOOP DICTIONARIES
+
+#using for loop
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+
+# printing all values
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(thisdict[x])  
+
+#values() ----> to return the values of the dictonary in loop
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.values():
+  print(x)
+
+# keys() ----> to return the keys of the  dictionary in loop
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.keys():
+  print(x)
+
+#items() ----> to get both the keys and values through loop
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x,y in thisdict.items():
+  print(x, y)
+
+
+#COPY DICTIONARIES  
+
+#copy() ----> make a copy of a dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+#dict() ----> also dict is used to copy
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+
+#NESTED DICTIONARIES
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+print(myfamily)
+
+#Access Items in Nested Dictionaries
+print(myfamily["child1"]['name'])
+
+#loop through nested dictionaries
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+for x, obj in myfamily.items():
+    print(x)
+    
+    for y in obj:
+        print(y + ':', obj[y])
---
+++ tuples.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+#tuple length
+print(len(thistuple))
+
+#creating tuple with one item----> if this comma is not present python won't recognize it as tuple
+thistuple = ("apple",)
+print(type(thistuple))
+
+# The tuple() Constructor
+thistuple = tuple(("apple",))
